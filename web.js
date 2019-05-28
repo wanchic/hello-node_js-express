1f8e9b7ac175eeb7d148ff44a6d067a05d144281
Eric Wanchic <eric@gratiel.com>
2019-05-27 20:05:54 -0500

    Simple Text #2
    Created to test Heroku - AutoDeplyment.

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('hello, Mr. Wanchic');
+  res.send('-- I\'m going to Auto-Deploy now.');
 });
 
 app.listen(process.env.PORT || 3000);
