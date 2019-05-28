79c50a711354a3fd2b9aea41df9c8f89fe197fc8
Eric Wanchic <eric@gratiel.com>
2019-05-27 20:09:56 -0500

    Simple Text
    Test Heroku AutoDeploy, removed CI wait.

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('hello, Mr. Wanchic');
-  res.send('-- I\'m going to Auto-Deploy now.');
+  res.send('-- I\'m going to Auto-Deploy now. +1');
 });
 
 app.listen(process.env.PORT || 3000);
